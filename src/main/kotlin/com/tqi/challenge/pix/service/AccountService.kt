11aa2953430e2f00de57230e3db479c4190494de
ilanazare <ivanazare94@gmail.com>
2024-04-24 15:57:09 -0300

    simplifles exceptions with error

--- src/main/kotlin/com/tqi/challenge/pix/service/AccountService.kt
+++ src/main/kotlin/com/tqi/challenge/pix/service/AccountService.kt
@@ -1,7 +1,6 @@
 package com.tqi.challenge.pix.service
 
 import com.tqi.challenge.pix.domain.entity.Accounts
-import com.tqi.challenge.pix.exception.ListAccountNotFound
 import com.tqi.challenge.pix.repository.AccountRepository
 import com.tqi.challenge.pix.web.request.RetrieveAccountRequest
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,11 +11,7 @@ class AccountService(
     @Autowired
     val accountRepository: AccountRepository,
 ) {
-    fun findListAccountByCustomer(customer: String) =
-        accountRepository.findListAccountByCustomer(customer)
-            .takeIf {
-                it.isNotEmpty()
-            } ?: ListAccountNotFound("List account not found for $customer")
+    fun findListAccountByCustomer(customer: String) = accountRepository.findListAccountByCustomer(customer)
 
     fun create(request: Accounts): RetrieveAccountRequest {
         if (!accountRepository.findAccountByAccount(request.customer).isPresent) {
