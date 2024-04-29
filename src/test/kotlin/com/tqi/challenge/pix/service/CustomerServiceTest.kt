e837051c2e1a809a78ddd2abad274bd49830dba5
ilanazare <ivanazare94@gmail.com>
2024-04-29 11:11:04 -0300

    includes tests customer update successfully

--- src/test/kotlin/com/tqi/challenge/pix/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/tqi/challenge/pix/service/CustomerServiceTest.kt
@@ -4,6 +4,8 @@ import com.tqi.challenge.pix.domain.entity.Accounts
 import com.tqi.challenge.pix.domain.entity.Customers
 import com.tqi.challenge.pix.domain.entity.Payment
 import com.tqi.challenge.pix.repository.CustomerRepository
+import com.tqi.challenge.pix.web.request.CustomerRequest
+import com.tqi.challenge.pix.web.request.toCustomerRequest
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
@@ -53,6 +55,19 @@ class CustomerServiceTest(
 
     @Test
     fun `tests customer update successfully `() {
+        val customer = "23454345090"
+        val request = buildUpdateCustomerRequest()
+
+        every {
+            customerRepository.findCustomerByCustomer(customer)
+        } returns buildUpdateCustomerResponse()
+        every {
+            customerRepository.save(request.toCustomerRequest())
+        } returns buildUpdateCustomerResponse()
+
+        customerService.update(request.toCustomerRequest())
+
+        assertEquals(request.name, "iva luis nazareth")
     }
 
     private fun buildCustomer() =
@@ -79,6 +94,60 @@ class CustomerServiceTest(
             customer = "23454345090",
             account = "23456-8",
             pixType = enumValueOf("CPF"),
+            pixKey = "77872339533",
+        )
+
+    private fun buildUpdateCustomerRequest() =
+        CustomerRequest(
+            customer = "23454345090",
+            name = "iva luis nazareth",
+            cpf = "77872339587",
+            email = "iva@hotmail",
+            phone = "71984376654",
+            account = listOf(buildUpdateAccountRequest()),
+        )
+
+    private fun buildUpdateAccountRequest() =
+        Accounts(
+            customer = "23454345090",
+            bank = enumValueOf("PAG_BANK"),
+            agency = "23456-8",
+            account = "5676543-0",
+            payment = listOf(buildUpdatePaymentRequest()),
+        )
+
+    private fun buildUpdatePaymentRequest() =
+        Payment(
+            customer = "23454345090",
+            account = "23456-8",
+            pixType = enumValueOf("PHONE_NUMBER"),
+            pixKey = "77872339533",
+        )
+
+    private fun buildUpdateCustomerResponse() =
+        Customers(
+            customer = "23454345090",
+            name = "iva luis nazareth",
+            cpf = "77872339587",
+            email = "iva@hotmail",
+            phone = "71984376654",
+            account = listOf(buildUpdateAccountResponse()),
+        )
+
+    private fun buildUpdateAccountResponse() =
+        Accounts(
+            customer = "23454345090",
+            bank = enumValueOf("PAG_BANK"),
+            agency = "23456-8",
+            account = "5676543-0",
+            payment = listOf(buildUpdatePaymentResponse()),
+        )
+
+    private fun buildUpdatePaymentResponse() =
+        Payment(
+            customer = "23454345090",
+            account = "23456-8",
+            pixType = enumValueOf("PHONE_NUMBER"),
             pixKey = "77872339533",
         )
 }
